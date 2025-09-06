4738e3669abc771ab937576449feb4a864ab1403
Mayansh Bangali <mayanshbangali49@gmail.com>
2025-09-06 15:35:52 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,8 @@ const app = express();
 const getAllowedOrigins = () => {
   const origins = [
     'http://localhost:5000',
-    'http://127.0.0.1:5000'
+    'http://127.0.0.1:5000',
+    'https://vestira.onrender.com/'
   ];
   
   // Allow any Replit domain
